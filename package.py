29ff01508e2213e28d47a8f1b484e5028db8aeae
Benjamin Skinner <benjamin.skinner96@gmail.com>
2020-10-07 15:04:11 -0400

    Fixed windows compiler and version delimiter

--- package.py
+++ package.py
@@ -1,14 +1,22 @@
 name = 'zlib'
 
-version = '1.2.11+ta.1.0.1'
+version = '1.2.11-ta.1.1.0'
 
 authors = [
     'benjamin.skinner',
 ]
 
 variants = [
-    ['platform-windows', 'arch-x64', 'os-windows-10', 'visual_studio-2017.15.9'],
+    ['platform-windows', 'arch-x64', 'os-windows-10'],
 ]
+
+@early()
+def private_build_requires():
+    import sys
+    if 'win' in str(sys.platform):
+        return ['visual_studio']
+    else:
+        return ['gcc-7']
 
 build_system = "cmake"
 
@@ -20,6 +28,7 @@ def commands():
     env.ZLIB_PACKAGE_VERSION.set(split_versions[1])
 
     env.ZLIB_ROOT.set( "{root}" )
+    env.ZLIB_ROOT_DIR.set( "{root}" )
     env.ZLIB_LIB_DIR.set( "{root}/lib" )
     env.ZLIB_INCLUDE_DIR.set( "{root}/include" )
 
